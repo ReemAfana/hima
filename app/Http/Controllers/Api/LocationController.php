3985e96c7a979a309edc286ed8c2c92429c535e8
ReemAfana <reemafana2@gmail.com>
2026-04-13 17:40:20 +0200

    Feature: Governorates and cities - location system complete

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\City;
+use App\Models\Governorate;
 
 class LocationController extends Controller
 {
-    //
+    // Get all governorates
+    public function governorates()
+    {
+        return response()->json(Governorate::all());
+    }
+
+    // Get cities by governorate
+    public function cities($governorateId)
+    {
+        $cities = City::where('governorate_id', $governorateId)->get();
+        return response()->json($cities);
+    }
 }
